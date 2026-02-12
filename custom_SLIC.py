ecf67ae8766e8be95e06748537e8ee155cd8aa41
motavini <viniciusmatamota08@gmail.com>
2026-02-12 11:15:31 +0100

    New connectivity enforcing algorithm

--- custom_SLIC.py
+++ custom_SLIC.py
@@ -1,7 +1,7 @@
 import cv2 as cv
 import numpy as np
 
-def custom_SLIC(image_file, K, m):
+def SLIC(image_file, K, m):
     image = cv.imread(image_file)
 
     # Convert to LAB color space
@@ -23,7 +23,7 @@ def custom_SLIC(image_file, K, m):
         centers = update_cluster_centers(L, a, b, labels, centers)
 
     labels = assign_to_clusters(L, a, b, centers, S, m)
-    #labels = enforce_connectivity(labels)
+    labels = enforce_connectivity(labels)
 
     print(labels[:3*S, :3*S])  # Debug
 
@@ -95,49 +95,49 @@ def init_cluster_centers(L, a, b, S):
     return center
 
 def enforce_connectivity(labels):
-    cluster_sizes = np.bincount(labels.flatten())
-    for i in range(len(cluster_sizes)):
-        mask = (labels == i)
+    label_list = np.unique(labels)
+    for label in label_list:
+        mask = (labels == label)
         num_disjoint , disjoint_labels = cv.connectedComponents(mask.astype(np.uint8))
         if num_disjoint > 2:
+            disjoint_sizes = {j: np.sum(disjoint_labels == j) for j in range(1, num_disjoint)}
+            max_size = max(disjoint_sizes.values())
             for j in range(1, num_disjoint):
-                if np.sum(disjoint_labels == j) < cluster_sizes[i] / 4:
+                if disjoint_sizes[j] < max_size:
                     labels[disjoint_labels == j] = -1
 
     stray_pixels = (labels == -1)
+    label_sizes = {label: np.sum(labels == label) for label in label_list}
     num_stray, stray_labels = cv.connectedComponents(stray_pixels.astype(np.uint8))
     for i in range(1, num_stray):
         mask = (stray_labels == i)
-        max_label = largest_neighbour(mask, labels)
+        max_label = largest_neighbour(mask, labels, label_list, label_sizes)
         labels[mask] = max_label
 
     return labels
 
-def largest_neighbour(mask, labels):
-    label_list = np.unique(labels[labels != -1])
-    label_sizes = {label: np.sum(labels == label) for label in label_list}
+def largest_neighbour(mask, labels, label_list, label_sizes):
     max_size = 0
     max_label = -1
-    for label in label_list:
-        if label in neighbour_labels(mask, labels):
-            if label_sizes[label] > max_size:
-                max_size = label_sizes[label]
-                max_label = label
+    neighbours = neighbour_labels(mask, labels)
+    for label in neighbours:
+        if label_sizes[label] > max_size:
+            max_size = label_sizes[label]
+            max_label = label
     return max_label
 
 def neighbour_labels(mask, labels):
-    label_list = np.unique(labels[labels != -1])
-    neighbours = set()
-    for label in label_list:
-        joint = np.ma.mask_or(mask, labels == label)
-        num_joint_cc, _ = cv.connectedComponents(joint.astype(np.uint8))
-        if num_joint_cc == 2:       # connectedComponentes returns the background as a component, so 2 means they are connected
-            neighbours.add(label)
-    return neighbours
+    kernel = np.ones((3,3), np.uint8)
+    dilated = cv.dilate(mask.astype(np.uint8), kernel, iterations=1)
+    border = dilated.astype(bool) & (~mask)
+    neighbor_labels = np.unique(labels[border])
+    neighbor_labels = neighbor_labels[neighbor_labels != -1]
+
+    return neighbor_labels
 
 
 if __name__ == "__main__":
     image_file = "lena_color.tiff"
     K = 100 
     m = 10  
-    custom_SLIC(image_file, K, m)
+    SLIC(image_file, K, m)
